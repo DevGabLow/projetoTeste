9912d58e87d02bd168e369849b44588d4284b96c
Gabriel Falcão <gabrielfdamaceno@gmail.com>
2020-07-27 14:12:48 -0300

    adsw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ import { NewItemComponent } from './list-items/new-item/new-item.component';
 
 
 const routes: Routes = [
-      {path: '', redirectTo: 'login'},
+      {path: '', redirectTo: 'login', pathMatch: 'full'},
       {path: 'login', component: LoginComponent},     
       {path: 'users',component: UsersComponent,canActivate: [AuthGuardService]},
       {path: 'listItems', component: ListItemsComponent, canActivate: [AuthGuardService]},
